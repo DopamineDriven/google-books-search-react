3321b45c4502217cd4b71ada681e2bfc22f71351
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-27 02:51:09 -0500

    search books cont

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import API from "../utils/API.js";
 
 class Search extends Component {
     state = {
@@ -8,11 +8,49 @@ class Search extends Component {
         errorMsg: 'Input Book Title'
      }
 
-     handleInputDelta = event => {
+    handleInputDelta = event => {
         const { name, value } = event.target;
         this.setState({
             [name]: value
         })
+    }
+
+    fetchBooksGoogle = () => {
+        if (this.state.query) {
+            API.getBooksGoogle(this.state.query)
+                .then(res => {
+                    this.setState({
+                        books: res.data.items
+                    })
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.setState({
+                        books: [],
+                        errorMsg: "No books found related to your query"
+                    })
+                })
+        }
+    }
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.fetchBooksGoogle();
+    }
+
+    handleSaveBook = id => {
+        const book = this.state.books.find(book => book.id === id)
+
+        API.saveBook({
+            googleId: book.id,
+            title: book.volumeInfo.title,
+            subtitle: book.volumeInfo.subtitle,
+            authors: book.volumeInfo.authors,
+            link: book.volumeInfo.previewLink,
+            date: book.volumeInfo.publishedDate,
+            description: book.volumeInfo.description,
+            image: book.volumeInfo.imageLinks.thumbnail
+        }).then(() => this.getBooks())
     }
 
      render() {
